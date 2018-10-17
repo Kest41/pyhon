fe9f6778c2f18d4f886483c8d8b5b43c4eaa93e1
Kest41 <k.v.kozlov@yandex.ru>
2018-10-17 17:26:53 +0700

    Вариант без флагов

--- generate_permutations.py
+++ generate_permutations.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def find(number, A):
+	""" Ищет x в A и возвращает true, если такой есть,
+		False, если такого нет
+	"""
+	for x in A:
+		if number == x:
+			return True
+		return False
+
 def generate_permitations(N:int, M:int=-1, prefix=None):
 	""" Генерация всех перестановок N чисел в M позициях,
 		с префиксом prefix
@@ -8,3 +20,8 @@ def generate_permitations(N:int, M:int=-1, prefix=None):
 		print(prefix)
 		return
 	for number in range(1, N+1):
+		if find(number, prefix): prefix:
+			continue
+		prefix.append(number)
+		generate-permutations(N, M-1, prefix)
+		prefix.pop()
